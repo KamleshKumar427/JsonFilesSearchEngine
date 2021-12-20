336ee376ba8c308ac6505e8644838226009184bd
domer20 <93879986+domer20@users.noreply.github.com>
2021-12-19 22:17:56 -0800

    solved title problem, added position based ranking
    titles were not being parsed into forward index, fixed
    basic ranking based on position of words in article (not on distances)

--- main.py
+++ main.py
@@ -19,23 +19,13 @@ url_id = 0
 
 for file_name in os.listdir():      # for every file in the sample_data directory
     with open(file_name, "rb") as f:  # opening each file
-        objects = ijson.items(f, 'item.content')   # loading only the contents of each article into memory
+        objects = ijson.items(f, 'item')   # loading only the contents of each article into memory
         for data in objects:   # going through the contents of each article
+            data = data['title'] + data['content']
             words_list = re.split(':|;|,|\s|\n|&|\?|\t|-|/|\|@|"|!', data)  # splitting the contents into words using multiple delimiters
             for word in words_list:  # going through the list of words
                 temp = "".join(c for c in word if c.isalpha() or c == '.') # keeping only the alphabet and . in each word
                 if temp and len(temp) > 2 and temp.lower() not in stop_words:   # filtering
-                    tmp = stemmer.stem(temp.lower())  # stemming the filtered words.
-                    if tmp not in Lex_dict:  # if the word is not in lexicon
-                        Lex_dict[tmp] = word_id  # insert it in lexicon
-                        word_id += 1
-    with open(file_name, "rb") as f:
-        titles = ijson.items(f, 'item.title') # loading title of each file into memory
-        for title in titles:      # for the title of every article
-            words_list = title.split()
-            for word in words_list:  # going through the list of words
-                temp = "".join(c for c in word if c.isalnum() or c == '.')
-                if temp and len(temp) > 2 and temp.lower() not in stop_words:
                     tmp = stemmer.stem(temp.lower())  # stemming the filtered words.
                     if tmp not in Lex_dict:  # if the word is not in lexicon
                         Lex_dict[tmp] = word_id  # insert it in lexicon
@@ -48,9 +38,10 @@ forwardIndex = {}                       # dictionary for complete forward index:
 forwardIndexWords = {}     # dictionary to store word_ids and hit lists for each article
 for file_name in os.listdir():                          # for every file in the sample_data directory
     with open(file_name, "rb") as f:
-        objects = ijson.items(f, 'item.content')  # loading contents of each article into memory
+        objects = ijson.items(f, 'item')  # loading contents of each article into memory
         for data in objects:                                  # for every article in the file
-            words_list = re.split(':|;|,|\s|\n|&|\?|\t|-|/|\|@|"|!', data)
+            data = data['title'] + data['content']
+            words_list = re.split(':|;|,|\s|\n|&|\?|\t|-|/|\|@|"|!', str(data))
             forwardIndexWords.clear()
             for index, word in enumerate(words_list):  # going through the whole list of words
                 temp = "".join(c for c in word if c.isalpha() or c == '.')
@@ -91,46 +82,39 @@ print(f" Now Runtime of the program is {end - start}")
 
 query = input("Enter your search query: ")
 terms = query.split()
-print(terms)
 
 docs = {}
 docs_list = []
 total_docs = set()
 temp_docs = {}
-number = 0
 
 for word in terms:
     temp = "".join(c for c in word if c.isalpha() or c == '.')
     tmp = stemmer.stem(temp.lower())
     if tmp in Lex_dict:
         word_id = int(Lex_dict[tmp])
-        print(word_id)
         docs = Inv_index[str(word_id)]
         for doc_id in docs:
             freq = len(Inv_index[str(word_id)][str(doc_id)])
-            print(str(number) + ". " + str(freq))
-            number += 1
+            for pos in Inv_index[str(word_id)][str(doc_id)]:
+                if pos < 20:
+                    freq += 10
             temp_docs[str(doc_id)] = int(freq)
     docs_list.append(copy.deepcopy(temp_docs))
     temp_docs.clear()
 
 for doc_list in docs_list:
     total_docs = set(docs_list[0].keys()).intersection(set(doc_list.keys()))
-    print(total_docs)
 
 end1 = time.time()
 print(f" Now Runtime of the program is {end1 - end}")
 doc_dict = {}
 freq_sum = 0
 for doc in total_docs:
-    print(doc)
-    freq_sum = sum([x[str(doc)] for x in docs_list if doc in x])
+    freq_sum = sum([x[str(doc)] for x in docs_list])
     doc_dict[str(doc)] = freq_sum
-    freq_sum = 0
+
 doc_dict = sorted(doc_dict.items(), key=lambda x: -x[1])
-print(doc_dict)
 for index, doc1 in enumerate(doc_dict):
     print(doc1)
     print(str(index) + " " + doc_ref[doc1[0]])
-
-
